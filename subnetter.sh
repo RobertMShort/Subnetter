ec1fb0e4e3ba3984b83a4632a8119c7af7debf12
Robert Short <134110968+RobertMShort@users.noreply.github.com>
2023-06-10 01:41:13 -0400

    Update subnetter.sh

--- subnetter.sh
+++ subnetter.sh
@@ -13,33 +13,15 @@ subnetmask=([23]=255.255.254.0 [24]=255.255.255.0 [25]=255.255.255.128)
 declare -A binary
 binary=([23]=11111111.11111111.11111110.00000000 [24]=11111111.11111111.11111111.00000000 [25]=11111111.11111111.11111111.10000000)
 
-declare -A ips
-ips=([23]=9 [24]=8 [25]=7)
-
 CIDR=${ip: -2}
 
+hosts=$(echo "${binary[$CIDR]}" | grep -o '0' | wc -l)
+networks=$(echo ${binary[$CIDR]: -8} | grep -o '1' | wc -l)
+
 if [ -v subnetmask[$CIDR] ]; then
-	echo "Subnet Mask: ${subnetmask[$CIDR]}"
-	echo "Subnet Binary: ${binary[$CIDR]}"
-	echo "IP Addresses: "$((2**${ips[$CIDR]}))
-	echo "Usable IP Addresses: "$((2**${ips[$CIDR]}-2))
+        echo "Subnet Mask: ${subnetmask[$CIDR]}"
+        echo "Subnet Binary: ${binary[$CIDR]}"
+        echo "IP Addresses: "$((2**"$hosts"))
+        echo "Usable IP Addresses: "$((2**"$hosts"-2))
+        echo "Networks: "$((2**"$networks"))
 fi
-
-case $CIDR in
-
-	"23")
-		echo "Networks: 2"
-		;;
-		
-	"24")
-		echo "Networks: 1"
-		;;
-
-	"25")
-        echo "Networks: 1/2"
-		;;
-
-	*)
-		echo "Invalid IP"
-		;;
-esac
